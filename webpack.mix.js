8cf327024fd07cc22321135fd46c06c8bdc759ef
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-01-19 23:48:24 +0200

    feat: mobile nav, video popup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.postCss('src/app.css', 'static/app.css', [
-  require("tailwindcss"),
-])
+mix.js('src/app.js', 'static/app.js')
+  .postCss('src/app.css', 'static/app.css', [
+    require("tailwindcss"),
+  ])
